f3710120cd848f58c4ab2d196570f44ed7f98429
ValentinB57 <112543676+ValentinB57@users.noreply.github.com>
2023-07-20 10:03:41 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,6 +11,22 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/ValentinB57/devops.git'
             }
         }
+        stage('Run Tests') {
+            steps {
+                sh "grep -q '<h1>Fromage</h1>' /chemin/vers/index.html"
+            }
+            post {
+                always {
+                    echo 'Fin des tests.'
+                }
+                success {
+                    echo 'Le test a réussi !'
+                }
+                failure {
+                    echo 'Le test a échoué :('
+                }
+            }
+        }
     }
 }
 
